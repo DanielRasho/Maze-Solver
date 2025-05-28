ce201254cfa679832bf9c75ee2ab0f47d8d5aa11
ElrohirGT <elrohirgt@gmail.com>
2025-05-27 20:39:49 -0600

    feat: Added main.ts types

--- solver/main.ts
+++ solver/main.ts
@@ -1,28 +1,45 @@
 import neo4j, { Driver, Session } from "npm:neo4j-driver";
 
-const uri = "bolt://localhost:7687";
-const username = "neo4j";
-const password = "6OV31H6Q5NdSCqGatBtpV27Mcd-gKctC99ZzFEAE1H0";
+// const uri = "bolt://localhost:7687";
+// const username = "neo4j";
+// const password = "6OV31H6Q5NdSCqGatBtpV27Mcd-gKctC99ZzFEAE1H0";
+//
+// const driver: Driver = neo4j.driver(uri, neo4j.auth.basic(username, password));
+//
+// async function executeQuery(query: string, params: object) {
+// 	const session: Session = driver.session();
+// 	try {
+// 		const result = await session.executeRead((tx) => tx.run(query, params));
+// 		return result.records;
+// 	} finally {
+// 		await session.close();
+// 	}
+// }
+//
 
-const driver: Driver = neo4j.driver(uri, neo4j.auth.basic(username, password));
+type Connection = {
+	Piece: PuzzlePiece;
+	Where: string;
+};
 
-async function executeQuery(query: string, params: object) {
-	const session: Session = driver.session();
-	try {
-		const result = await session.executeRead((tx) => tx.run(query, params));
-		return result.records;
-	} finally {
-		await session.close();
-	}
-}
+type PuzzlePiece = {
+	Id: string;
+	IsMissing: boolean;
+	Connections: Connection[];
+};
+
+type PuzzleInfo = {
+	Tematica: string;
+	Material: string;
+};
 
 async function main() {
-	const query = "MATCH (n:Person {name: $name}) RETURN n";
-	const params = { name: "Alice" };
-	const records = await executeQuery(query, params);
-	console.log(records);
+	// const query = "MATCH (n:Person {name: $name}) RETURN n";
+	// const params = { name: "Alice" };
+	// const records = await executeQuery(query, params);
+	console.log("Hello World!");
+	// console.log(records);
 }
 
-main()
-	.catch(console.error)
-	.finally(() => driver.close());
+main().catch(console.error);
+// .finally(() => driver.close());
