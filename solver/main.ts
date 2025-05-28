1f1941e323e1870e4b3bb158c4abbdeb6be5d189
ElrohirGT <elrohirgt@gmail.com>
2025-05-28 10:44:02 -0600

    feat: Added improved path and BFS

--- solver/main.ts
+++ solver/main.ts
@@ -16,27 +16,13 @@ async function executeQuery(query: string, params: object) {
 	}
 }
 
-type Connection = {
-	FromPort: string;
-	ToPort: string;
-	ConnectedPiece: PuzzlePiece;
-};
-
-type PuzzlePiece = {
-	Id: string;
-	IsMissing: boolean;
-	Connections: Connection[];
-};
-
-type PuzzleInfo = {
-	Tematica: string;
-	Material: string;
-};
-
 async function main() {
+	// TODO: Uncomment this...
 	// const puzzleId = prompt("What puzzle do you whish to solve?");
 	const puzzleId = "realOctopus";
 	const pieceId = parseInt(prompt("What piece is your starting point?"));
+
+	// TODO: Check if pieceId is not a missing piece...
 
 	const query = `
 MATCH (cs:Piece {id: $pieceId}) -[:BELONGS]-> (:Puzzle {custom_id: $puzzleId})
@@ -44,12 +30,41 @@ CALL apoc.path.expandConfig(cs,{relationshipFilter:'CONNECTED>',uniqueness:'NODE
 RETURN path`;
 	const params = { pieceId, puzzleId };
 	const records: any[] = await executeQuery(query, params);
-	records.forEach((r) => {
-		console.log(r.get("path"));
-	});
-	// console.log(records);
 
-	// const root = constructTree(records, startingPieceId);
+	let missingPieces = new Set<string>();
+	for (let i = 1; i < records.length; i++) {
+		const record = records[i];
+		const segments = record.get("path").segments;
+		const lastPathSegment = segments[segments.length - 1];
+
+		if (lastPathSegment.start.properties.isMissing) {
+			missingPieces.add(lastPathSegment.start.properties.id.low);
+			continue;
+		}
+		if (lastPathSegment.end.properties.isMissing) {
+			missingPieces.add(lastPathSegment.end.properties.id.low);
+			continue;
+		}
+
+		// TODO: Prettify this logging...
+		console.log(
+			"Connecte el puerto",
+			lastPathSegment.relationship.properties.fromPort.low,
+			"de la pieza",
+			lastPathSegment.start.properties.id.low,
+			"al puerto",
+			lastPathSegment.relationship.properties.toPort.low,
+			"de la pieza",
+			lastPathSegment.end.properties.id.low,
+		);
+	}
+
+	if (missingPieces.size > 0) {
+		console.log("WARNING: Tienes piezas faltantes!");
+	}
+	for (const id of missingPieces) {
+		console.log("-", id);
+	}
 }
 
 main()
